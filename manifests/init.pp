4519602c797b628a582750731d64cdaf60d1c438
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-10-07 23:38:55 +0200

    other method...

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,27 +5,28 @@ class spotify($version='0.8.5.1333') {
     source   => "https://www.dropbox.com/s/b4ml7cdaquzw07h/Spotify_${version}.dmg"
   }
 
-  file {'Spotify_new_archive':
-      ensure  => present,
-      path    => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive",
-      content => '',
-      require => Package['spotify']
+  exec { "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive":
+    cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
+    creates => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive",
+    path    => ["/usr/bin", "/usr/sbin"]
   }
 
-  file {'Spotify_new_archive_sig':
-      ensure  => present,
-      path    => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig",
-      content => '',
-      require => Package['spotify']
+  exec { "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig":
+    cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
+    creates => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig",
+    path    => ["/usr/bin", "/usr/sbin"],
+    unless  => 'test -e /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig'
   }
 
-  exec { 'spotify_setup_1':
-    command => "chflags uchg /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive",
+  exec { "chflags uchg /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive":
+    cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
+    path    => ["/usr/bin", "/usr/sbin"],
     require => File['Spotify_new_archive']
   }
 
-  exec { 'spotify_setup_2':
-    command => "chflags uchg /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig",
+  exec { "chflags uchg /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig":
+    cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
+    path    => ["/usr/bin", "/usr/sbin"],
     require => File['Spotify_new_archive_sig']
   }
 }
