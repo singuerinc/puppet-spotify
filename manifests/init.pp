d5c116c8bf64bb1040f06d7bd4d2f4a7967a99e0
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-10-07 23:45:20 +0200

    ooooootra más

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,13 +5,15 @@ class spotify($version='0.8.5.1333') {
     source   => "https://www.dropbox.com/s/b4ml7cdaquzw07h/Spotify_${version}.dmg"
   }
 
-  exec { "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive":
+  exec { "Spotify_new_archive":
+    command => "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive",
     cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
     creates => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive",
     path    => ["/usr/bin", "/usr/sbin"]
   }
 
-  exec { "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig":
+  exec { "Spotify_new_archive_sig":
+    command => "touch /Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig",
     cwd     => "/Users/${::luser}/Library/Application Support/Spotify",
     creates => "/Users/${::luser}/Library/Application Support/Spotify/Spotify_new.archive.sig",
     path    => ["/usr/bin", "/usr/sbin"],
