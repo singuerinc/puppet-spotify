016a027455ba53e6eece7b260e0d57677ceaf6cf
Nahuel Scotti <nahuel.scotti@gmail.com>
2014-03-09 22:02:08 +0100

    Update spotify_spec.rb

--- spec/classes/spotify_spec.rb
+++ spec/classes/spotify_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 describe 'spotify' do
 
-  version = '0.8.5.1333'
-
   it do
     should contain_package("spotify").with({
-        :provider => 'appdmg_eula',
-        :source   => "https://www.dropbox.com/s/b4ml7cdaquzw07h/Spotify_#{version}.dmg"
+        :provider => 'appdmg',
+        :source   => "https://dl.dropboxusercontent.com/u/311265/apps/Spotify.dmg"
     })
   end
 end
